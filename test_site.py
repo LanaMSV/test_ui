bbce2735c2541a7f771ee1085ac894455d462967
LanaMSV <metla-na@mail.ru>
2023-06-29 00:11:12 +0300

    Updated  test_site.py

--- test_site.py
+++ test_site.py
@@ -1,10 +1,10 @@
 # libraries
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
 # constants
 URL='https://www.saucedemo.com/'
@@ -12,11 +12,20 @@ NAME='standard_user'
 PASSWORD='secret_sauce'
 
 # functions
+def get_driver():
+    chrome_options = Options()
+    chrome_options.add_argument("--window-size=1920,800")
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),options= chrome_options)
+    driver.implicitly_wait(10)
+    return driver
+
+
 def get_element_by_id(driver,locator):
     return driver.find_element(By.ID, locator)
 
 def element_click(driver,locator):
-    element=get_element_by_id(driver,locator)
+    element = get_element_by_id(driver,locator)
     element.click()
 
 def element_send_keys(driver,locator,text):
@@ -33,8 +42,8 @@ def login(driver,name,password):
         element_send_keys(driver,'password',password)
         element_click(driver,'login-button')
 
-
 # code
+driver =get_driver()
 driver.get(URL)
 element_click(driver,'user-name')
 
